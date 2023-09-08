c0355e6ae01288bc962d2670f8a5b94d261082d5
dharani <dharani.gsd@gmail.com>
2023-09-08 16:01:09 +0530

    Added the new files

---
+++ Assign1.java
@@ -0,0 +1,26 @@
+package Basics;
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int m= 95;
+		
+		if(m > 90){
+			
+			System.out.println("A Grade");
+		}
+		else if(m > 75) {
+			
+            System.out.println("B Grade");			
+		}
+		
+		else if(m > 50)
+			
+			System.out.println("C Grade");
+		}
+
+	}
+
+
